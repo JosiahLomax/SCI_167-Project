fa27b46e0855081022d429819ec79e32ee911395
JosiahLomax <jdlomax1505@gmail.com>
2024-01-26 17:07:26 -0800

    Created the ScriptableObject script and added in 2D Package
    I was only able to create the script for the scriptableObject (no code written) and I added in the 2D Unity Package so that we can use the 2D tools.

---
+++ I&C 167 Quarter Project/Assets/Scripts/SOTroopDefinition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "CastleSeige/TroopDefinition")]  //Creates a menu item for the scriptable object witin Unity
+public class SOTroopDefinition : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
