dca2f9366e76679fec41d9b3efbec6c7f81cdbc3
JosiahLomax <jdlomax1505@gmail.com>
2024-01-27 16:49:12 -0800

    Created a Basic Scriptable Object
    I have created a basic scriptable object that holds a troop type's name, health, movement speed, and damage per attack. However, I am unsure of how to go from there

---
+++ I&C 167 Quarter Project/Assets/Scripts/TroopManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopManager : MonoBehaviour
+{
+    [SerializeField]
+    private SOTroopDefinition troopDefinition;
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
