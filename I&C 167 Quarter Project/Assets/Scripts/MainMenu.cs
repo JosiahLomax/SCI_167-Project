3b9718f39158ccfec017a6eaf1b77b44eee81838
Jacklyn Truong <77140250+nylkcaj@users.noreply.github.com>
2024-02-01 17:46:37 -0800

    WILL THIS WORK??

--- I&C 167 Quarter Project/Assets/Scripts/MainMenu.cs
+++ I&C 167 Quarter Project/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,8 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         // unless we want to have different levels later on then ill add this
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 1); 
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 1);
+        // HELLO IS THIS WORKING??
 
         SceneManager.LoadScene(1);
     }
