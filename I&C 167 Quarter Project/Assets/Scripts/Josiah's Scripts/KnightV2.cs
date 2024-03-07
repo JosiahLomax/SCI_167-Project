d679aadd76c7d3fbdfa60ce3b7e63bcc96215c56
JosiahLomax <jdlomax1505@gmail.com>
2024-03-06 18:33:18 -0800

    Readding the Changes I made to the KnightV2 Script
    Made some additions to the KnightV2 script so that the troop will either move towards the enemy castle or the enemy troop, depending on which it sees. However, we need to work out some kinks with the troop detection in particular.

--- I&C 167 Quarter Project/Assets/Scripts/Josiah's Scripts/KnightV2.cs
+++ I&C 167 Quarter Project/Assets/Scripts/Josiah's Scripts/KnightV2.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Code Written by Josiah Lomax
 public class KnightV2 : MonoBehaviour
 {
     [SerializeField]
@@ -12,9 +13,17 @@ public class KnightV2 : MonoBehaviour
     private float agroRange;
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private GameObject enemyCastle;
+    [SerializeField]
+    private GameObject enemyTroop;
 
+
+    private bool canSeePlayer = false;
+    private bool canSeeTroop = false;
     private bool facingRight = true;
-    private Transform enemyPlayerPos;
+
+   
 
     
     public LayerMask enemyPlayer2Layer, enemyTroopLayerP2;
@@ -32,39 +41,59 @@ public class KnightV2 : MonoBehaviour
         CheckForTroop();
     }
 
+    // Code that Checks for the Enemy's Castle
     void CheckForPlayer()
     {
         RaycastHit2D hitPlayer = Physics2D.Raycast(rayCast.transform.position, facingRight ? Vector2.right : Vector2.left, agroRange, enemyPlayer2Layer);
 
         if (hitPlayer.collider == true)
         {
+            canSeePlayer = true;
+            Debug.Log("Player Detected!");
             StartCoroutine(PlayerDetected()); 
+        }
+        else
+        {
+            canSeePlayer = false;
+            Debug.Log("Player Not Detected!");
+            StopMoveToPlayer();
         }
        
     }
 
-    IEnumerator PlayerDetected()
+   IEnumerator PlayerDetected()
     {
-        Debug.Log("Player Detected");
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
+        MoveToPlayer();
     }
+  
 
+
+    // Code that Checks for the Enemy's Troop
     void CheckForTroop()
     {
         RaycastHit2D hitTroop = Physics2D.Raycast(rayCast.transform.position, facingRight ? Vector2.right : Vector2.left, agroRange, enemyTroopLayerP2);
 
         if (hitTroop.collider == true)
         {
+            canSeeTroop = true;
+            Debug.Log("Troop Detected!");
             StartCoroutine(TroopDetected());
+        }
+        else
+        {
+            canSeeTroop = false;
+            Debug.Log("Troop Not Detected!");
+            StopMoveToTroop();
         }
     }
 
     IEnumerator TroopDetected()
     {
-        Debug.Log("Troop Detected!");
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
+        MoveToTroop();
     }
-
+  
     private void OnDrawGizmos()
     {
         Gizmos.DrawRay(rayCast.transform.position, (facingRight ? Vector2.right : Vector2.left) * agroRange);
@@ -72,13 +101,58 @@ public class KnightV2 : MonoBehaviour
 
 
 
+
+    // Code that Moves the Knight to the Enemy's Castle
     void MoveToPlayer()
     {
+       if(canSeePlayer == true)
+       {
+            if(transform.position.x < enemyCastle.transform.position.x)
+            {
+                knightRB.velocity = new Vector2(moveSpeed, 0);
+            }
+            else if(transform.position.x > enemyCastle.transform.position.x)
+            {
+                knightRB.velocity = new Vector2(-moveSpeed, 0);
+            }
+       }
        
     }
 
+
+    void StopMoveToPlayer()
+    {
+        if (canSeePlayer == false)
+        {
+            knightRB.velocity = new Vector2(0, 0);
+           // Debug.Log("Player Not Detected!");
+        }
+    }
+
+
+
+    // Code that moves the Knight to the Enemy's Troop
     void MoveToTroop()
     {
-
+        if(canSeeTroop == true)
+        {
+            if(transform.position.x < enemyTroop.transform.position.x)
+            {
+                knightRB.velocity = new Vector2(moveSpeed, 0);
+            }
+            else if( transform.position.x > enemyTroop.transform.position.x)
+            {
+                knightRB.velocity = new Vector2(-moveSpeed, 0);
+            }
+        }
     }
+
+    void StopMoveToTroop()
+    {
+        if(canSeeTroop == false)
+        {
+            knightRB.velocity = new Vector2(0, 0);
+        }
+    }
+   
 }
