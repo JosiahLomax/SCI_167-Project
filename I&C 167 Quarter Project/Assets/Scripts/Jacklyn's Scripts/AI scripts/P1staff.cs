b11bcad10195d7b0303a89009a3245f06ffa59e8
Jacklyn Truong <77140250+nylkcaj@users.noreply.github.com>
2024-03-16 20:00:04 -0700

    YASSS THEYRE FIGHTINGG P1MAGE ADDED

---
+++ I&C 167 Quarter Project/Assets/Scripts/Jacklyn's Scripts/AI scripts/P1staff.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1staff : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject P1ballPrefab;
+
+    [SerializeField]
+    private float coolDown = 2.5f;
+
+    [SerializeField]
+    private float timer;
+
+    void Start()
+    {
+        timer = coolDown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Instantiate(P1ballPrefab, transform.position, Quaternion.identity);
+            timer = coolDown;
+        }
+
+    }
+}
